0eeee7c1db2ab194020ffb3c0c5ad1203b870f5a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-21 08:20:58 +0530

    State Variables items: State variable that stores an array of items fetched from the database. newItem: State variable that stores the value of the new item being added. selectedItemId: State variable that stores the id of the item currently selected for updating. updatedItemName: State variable that stores the updated name of the item being updated.

---
+++ src/CrudApp.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { db } from './firebase';
+
+function CrudApp() {
+    const [items, setItems] = useState([]);
+    const [newItem, setNewItem] = useState("");
+    const [selectedItemId, setSelectedItemId] = useState(null);
+    const [updatedItemName, setUpdatedItemName] = useState("");
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+}
+
+
