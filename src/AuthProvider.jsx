d5ce220684c8375910065450b79d39e59376b4b9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-21 08:14:06 +0530

    useEffect Hook onAuthStateChanged Listener: The useEffect hook is used with an empty dependency array to run only once when the component mounts. Subscription: It subscribes to the onAuthStateChanged event provided by Firebase auth. This event is triggered whenever the authentication state changes (e.g., when a user signs in or signs out). Update currentUser: When the authentication state changes, the callback function provided to onAuthStateChanged is called. If a user is authenticated and their email is verified, the currentUser state is updated with the user object; otherwise, it's set to null. Loading State: The loading state is set to false once the authentication state is determined, indicating that the authentication process is completed.

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -12,7 +12,17 @@ export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user && user.emailVerified) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
 
   
 
