80cac8bbeb0420c438a1ac7610bb3dcdc034e860
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-21 08:13:54 +0530

    State Variables currentUser State: A state variable currentUser is initialized with null. It will store the currently authenticated user. loading State: A state variable loading is initialized with true. It indicates whether the authentication process is ongoing or not.

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -9,7 +9,8 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-  
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   
 
