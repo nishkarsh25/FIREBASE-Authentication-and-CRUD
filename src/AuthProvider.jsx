f88a01447415b3b89f69e5e896a5cc0cfd4deb43
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-21 08:13:03 +0530

    AuthContext Context Creation: The AuthContext is created using the createContext function from React. It will hold the authentication-related data shared between components. useAuth Hook: The useAuth hook is exported. It allows components to access the authentication context and retrieve the current user information.

---
+++ src/AuthProvider.jsx
@@ -0,0 +1,9 @@
+// src/AuthProvider.js
+import React, { useEffect, useState, createContext, useContext } from 'react';
+import { auth } from './firebase';
+
+const AuthContext = createContext();
+
+
+
+
