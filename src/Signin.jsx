d30be3b925568d231e8fb70678775b17c18129a7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-21 07:54:48 +0530

    State Variables Email State: A state variable email is initialized with an empty string. It will store the user’s email input. Password State: Similarly, a state variable password is initialized with an empty string. It will store the user’s password input.

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { auth } from './firebase';
 
 function Signin() {
-  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   
 
