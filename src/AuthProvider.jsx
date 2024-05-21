31fed25db486649d390c7f9ee75e371158ce7512
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-21 08:14:22 +0530

    AuthContext.Provider Provider Component: The AuthProvider component returns an AuthContext.Provider component. Value Prop: The value prop of the provider is set to an object containing the currentUser. This object will be accessible to any descendant components that use the useAuth hook. Conditional Rendering: The children components wrapped within the AuthProvider are rendered only when the loading state is false. This ensures that the child components wait until the authentication process is completed before rendering.

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -24,7 +24,7 @@ export function AuthProvider({ children }) {
     return unsubscribe;
   }, []);
 
-  
+  const value = { currentUser };
 
-  
+  return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
 }
